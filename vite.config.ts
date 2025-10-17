90910e890f4b5c4416a6002bb736590b0b031988
mosszhd <mustafakamalmoss@gmail.com>
2025-10-17 23:58:57 +0600

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the current mode
+  const env = loadEnv(mode, process.cwd(), ""); // third param: prefix filter ("" = no filter)
+
+  return {
+    base: env.VITE_BASE_PATH || "/landing_page", // use env variable
+    plugins: [react(), tailwindcss()],
+  };
 });
